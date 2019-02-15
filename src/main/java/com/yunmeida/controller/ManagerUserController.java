15809254ccf331cff06e79b4623a353fcc10f131
lx <394607573@qq.com>
2019-02-15 21:48:35 +0800

    添加ManagerBaseInfoController 修改ManagerUserController

--- src/main/java/com/yunmeida/controller/ManagerUserController.java
+++ src/main/java/com/yunmeida/controller/ManagerUserController.java
@@ -2,22 +2,23 @@ package com.yunmeida.controller;
 
 import com.yunmeida.entity.UserInfo;
 import com.yunmeida.service.ManagerUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/managerUser")
 public class ManagerUserController {
 
-    @Resource
+    @Autowired
     private ManagerUserService managerUserService;
 
     @RequestMapping("/login")
-    public String login(UserInfo user){
+    public String login(HttpServletRequest request, UserInfo user){
         UserInfo userInfo=managerUserService.findByName(user);
         if(userInfo!=null && userInfo.getPassword().equals(user.getPassword())){
+            request.setAttribute("login",user);
             return "index";
         }else{
             return "redirect:/login.jsp";
